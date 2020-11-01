b115b6e0419d71cb1b0493238b0d0e44fdfca1f3
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-01 20:45:28 +0900

    編集機能に係るedit&updateアクションを定義し、ビューを表示
    update_attributesメソッドは属性のハッシュを受け取り、成功時には更新と保存を続けて同時に行う。

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,6 +12,19 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    @list = List.find(params[:id])
+    if @list.update_attributes(list_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
+  end
+
   private
     def list_params
       params.require(:list).permit(:title).merge(user: current_user)

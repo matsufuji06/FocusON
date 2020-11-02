aeee1228c087197bd79d62723eb73ffc5289839c
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-03 08:30:01 +0900

    cardsのupdateアクションを定義

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -16,8 +16,14 @@ class CardsController < ApplicationController
   def edit
     @card = Card.find_by(id: params[:id])
   end
-
+  
   def update
+    @card = Card.find_by(id: params[:id])
+    if @card.update_attributes(card_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
   end
   
   def show

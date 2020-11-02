8f1b09bee60208b2f25c03a0149890bd4177a014
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-02 21:42:26 +0900

    cardsのcreateアクションを定義

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,5 +5,16 @@ class CardsController < ApplicationController
   end
 
   def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to :root
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:title, :memo, :list_id)
   end
 end
